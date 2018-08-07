b27b37d2f814273b4d171be98cb6ae1608ebdccc
marfloresrayon <psico.luzy@gmail.com>
2018-08-07 11:13:58 -0500

    Continuando funcionalidad de búsqueda

---
+++ js/firebase.js
@@ -0,0 +1,16 @@
+  // Initialize Firebase
+  window.fireRestaurantes = {
+    initializeFirebase: () => {
+  var config = {
+    apiKey: "AIzaSyCiLDBi-b0s3qQ7StQwIpepNwyEX65LHyo",
+    authDomain: "affamato-project.firebaseapp.com",
+    databaseURL: "https://affamato-project.firebaseio.com",
+    projectId: "affamato-project",
+    storageBucket: "affamato-project.appspot.com",
+    messagingSenderId: "371498322149"
+  };
+  firebase.initializeApp(config);
+},
+  };
+
+  
